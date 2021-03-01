aec04ae30528c85b1730c740f8c5475f9ae5c0b5
Satya Kommula <skommula@parimitha.com>
2021-03-01 16:07:48 +0530

    + sesssion ticket save and KeyShareExtensionMessage some config changes

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/message/extension/KeyShareExtensionMessage.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/message/extension/KeyShareExtensionMessage.java
@@ -41,10 +41,30 @@ public class KeyShareExtensionMessage extends ExtensionMessage {
     public KeyShareExtensionMessage(Config tlsConfig) {
         super(ExtensionType.KEY_SHARE);
         keyShareList = new LinkedList<>();
-        for (NamedGroup group : tlsConfig.getDefaultClientKeyShareNamedGroups()) {
-            if (group.isTls13() && NamedGroup.getImplemented().contains(group)) {
-                KeyShareEntry keyShareEntry = new KeyShareEntry(group, tlsConfig.getKeySharePrivate());
-                keyShareList.add(keyShareEntry);
+        /**
+         * for (NamedGroup group :
+         * tlsConfig.getDefaultClientKeyShareNamedGroups()) { if
+         * (group.isTls13() && NamedGroup.getImplemented().contains(group)) {
+         * KeyShareEntry keyShareEntry = new KeyShareEntry(group,
+         * tlsConfig.getKeySharePrivate()); keyShareList.add(keyShareEntry); } }
+         */
+
+        /**
+         * Added by Parimitha Technologies Pvt.Ltd.
+         */
+        if (tlsConfig.getCustomClientNamedGroups().size() > 0) {
+            for (NamedGroup group : tlsConfig.getCustomClientNamedGroups()) {
+                if (group.isTls13()) {
+                    KeyShareEntry keyShareEntry = new KeyShareEntry(group, tlsConfig.getKeySharePrivate());
+                    keyShareList.add(keyShareEntry);
+                }
+            }
+        } else {
+            for (NamedGroup group : tlsConfig.getDefaultClientKeyShareNamedGroups()) {
+                if (group.isTls13() && NamedGroup.getImplemented().contains(group)) {
+                    KeyShareEntry keyShareEntry = new KeyShareEntry(group, tlsConfig.getKeySharePrivate());
+                    keyShareList.add(keyShareEntry);
+                }
             }
         }
     }
